155e81325c857528d905d14dc40b0c852123dac2
iziuqo <iz.iuqo@gmail.com>
2024-10-24 16:08:32 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 // File: pages/_app.js
 import '../styles/globals.css'
+import { AuthProvider } from '../context/AuthContext'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
